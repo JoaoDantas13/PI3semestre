b8b7d7e00890415dc5894261a19b11b8af90ea05
João Vitor Dantas <dantasjoaovitor861@gmail.com>
2021-05-06 22:39:05 -0300

    vendas atualizado

--- src/main/java/br/senac/sp/speedracer/dao/VendasDAO.java
+++ src/main/java/br/senac/sp/speedracer/dao/VendasDAO.java
@@ -16,53 +16,57 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author Eduardo Silva
- */
+
+
 
 public class VendasDAO {
     
-    public static boolean cadastrar(Vendas vendas){
-        boolean ok = true;
-        String query = "insert into vendas (codprod, codcliente, produto,quantidade,valortotal) values (?,?,?,?,?)";
-        Connection con;
-        try {
-            con = Conexao.getConexao();
-            PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, vendas.getcodprod());
-            ps.setInt(2, vendas.getcodcliente());
-            ps.setDouble(3, vendas.getproduto());
-            ps.setDouble(4, vendas.getquantidade());
-            ps.setDouble(4, vendas.getvalortotal());
-            ps.executeUpdate();
-        } catch (SQLException ex) {
-            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
-            ok = false; 
-        }
-        return ok;        
-    } 
-    
-   public static List<Vendas> getVendas() {
-        List<Vendas> produtos = new ArrayList<>();
-        String query = "select * from vendas";
+        public static List<Vendas> getVendas() {
+               
+        List<Vendas> vendas = new ArrayList<>();
+        String query = "select * from Vendas";
         Connection con;
         try {
             con = Conexao.getConexao();
             PreparedStatement ps = con.prepareStatement(query);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()) {
-                int codprod = rs.getInt("codprod");
-                int codcliente = rs.getInt("codcliente");
-                String produto  = rs.getString("produto");
-                int quantidade  = rs.getInt("quantidade");
-                int valortotal  = rs.getInt("valortotal");
-                Vendas vendas = new Vendas(codprod, codcliente, produto,quantidade,valortotal);
-                vendas.add(vendas);
+            while (rs.next()) {
+                String PLACA = rs.getString("PLACA");
+                String NOME = rs.getString("NOME");
+                int QUANTIDADE = rs.getInt("QUANTIDADE");
+                double PRECOUNIT = rs.getDouble("PRECOUNIT");
+                /*Vendas vendas = new Vendas(PLACA, NOME, QUANTIDADE, PRECOUNIT);*/
+                /*vendas.add(vendas);*/
             }
         } catch (SQLException ex) {
-            Logger.getLogger(ProdutoDAO.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(VendasDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return produtos;
+        return vendas;
+
     }
+    
+    public static boolean cadastrar(Vendas vendas){
+        boolean ok = true;
+        String query = "insert into Vendas (placa, nome,quantidade,precounit) values (?,?,?,?)";
+        Connection con;
+        try {
+            con = Conexao.getConexao();
+            PreparedStatement ps = con.prepareStatement(query);
+            ps.setString(1, vendas.getPLACA());
+            ps.setString(2, vendas.getNOME());
+            ps.setInt(3, vendas.getQUANTIDADE());
+            ps.setDouble(4, vendas.getPRECOUNIT());
+            ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(VendasDAO.class.getName()).log(Level.SEVERE, null, ex);
+            ok = false; 
+        }
+        return ok;        
+    } 
+
+    
+
+    
 }
+
--- src/main/webapp/Vendas.jsp
+++ src/main/webapp/Vendas.jsp
@@ -6,24 +6,61 @@
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Vendas</title>
-    </head>
-    <body>
-        <h1>Vendas</h1>
-        <label>CodProd</label>
-        <input type="number">
-        <label>CodCliente</label>
-        <input type="number">
-        <label>CPF</label>
-        <input type="number"><br>
-        <label>Descrição do Produto</label>
-        <input type = "text"
-        <label>Quantidade</label>
-        <input type ="number"><br>
-        <button type ="button">Adicionar</button>
-        <button type ="button">Finalizar</button>
-    </body>
+<html lang="pt-br">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Vendas</title>
+ <link href="bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+</head>
+<body> 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<table width="60%" border="1" id="tabela-herdeiro" class="table">
+<tbody>
+    <tr>
+        <td>Placa<input type="text"></td>
+        <td>&nbsp;</td> 
+        <td width="10%">Nome<input type="text"></td></td>
+        <td width="27%">&nbsp;</td>
+        <td width="10%">Quantidade<input type="text"></td></td>
+        <td width="22%">&nbsp;</td>
+        <td width="10%">PrecoUnit<input type="text"></td></td>
+        <td width="22%">&nbsp;</td>
+    </tr>
+    
+    <tr>
+        <td colspan="6" align="right"><button class="btn btn-large btn-danger btn-xs" type="button">Remover</button></td>
+    </tr>       
+   
+</tbody>
+</table>
+<br>
+<button id="adicionar">Adicionar linha</button>
+</tbody>
+<script>
+    $(function(){
+
+   // já cria logo uma cópia do TBODY original
+   var copia = document.querySelector("#tabela-herdeiro tbody").outerHTML;
+
+    $(document).on("click", "#tabela-herdeiro button", function(){
+
+      var tr = $(this).closest("tbody");
+      
+      tr.fadeOut(400, function(){
+        this.remove(); 
+      }); 
+
+    });
+
+    $("#adicionar").on("click", function(){
+        $("#tabela-herdeiro").append(copia);
+    });          
+
+});
+</script>
+</body>
 </html>
